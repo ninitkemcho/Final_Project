ab30b00cce60ec52ed41eadf74ce44c6742e73b7
nini tkemcho <nino.tkemaladze@iset.ge>
2024-06-12 17:40:48 +0400

    Final +needs running

--- functions.py
+++ functions.py
@@ -15,37 +15,23 @@ class Person:
             'age': self.age,
             'weight': self.weight,
             'height': self.height,
-            'goal': self.goal
-            }
+            'goal': self.goal,
+            'password': self._password}
 
 class User(Person):
-    def __init__(self, name, age, weight, height, goal=None):
+    def __init__(self, name, age, weight, height, goal=None, password=None):
         super().__init__(name, age)
-        self.__weight = weight
-        self.__height = height
-        self.__goal = goal
-        self.__password = None
+        self.weight = weight
+        self.height = height
+        self.goal = goal
+        self._password = password
 
-    def get_weight(self):
-        return self.__weight
-
-    def set_weight(self, weight):
-        self.__weight = weight
-
-    def get_height(self):
-        return self.__height
-
-    def set_height(self, height):
-        self.__height = height
-
-    def get_goal(self):
-        return self.__goal
-
-    def set_goal(self, goal):
-        self.__goal = goal
-        
+    def get_password(self, password):
+        return self._password
+    
     def set_password(self, password):
-        self.__password = password
+        self._password = password
+       
         
         
 class validators:
@@ -70,6 +56,7 @@ class validators:
         except:
             print('\nInvalid input! Please enter date in correct format\n')
         return is_date, date
+
 
 
 class Workout:
@@ -118,15 +105,12 @@ class FitnessTracker:
     def __init__(self):
         self.users = {}
         self.workouts = []
-        self.df_workouts=pd.DataFrame(self.workouts)
-        self.df_users=pd.DataFrame(self.users).T
-        
-        
+        self.df_workouts = self.load_data()[0]
+        self.df_users = self.load_data()[1]
+              
     def authenticate_user(self, username, password):
-        if validators.user_validator(username, self.users):
-            user = self.users[username]
-            if user.get_password() == password:
-                return True
+        if validators.user_validator(username, self.users) and self.users[username]['password']==password:
+            return True
         return False    
     
     #Generating unique username for each person
@@ -135,7 +119,7 @@ class FitnessTracker:
             #Creating name+ random 4-digit number combination as username
             username=name+''.join(random.choice('0123456789') for _ in range(4))
             #Makes sure each username is unique
-            if username in self.users:
+            if username in self.df_users.index:
                 continue
             else:
                 break
@@ -143,26 +127,34 @@ class FitnessTracker:
     
     #Adding user info in users dict
     def create_user(self,  name, age, weight, height):
+        
+        #Generating username
         self.username=self.unique_username(name)
-        new_user=User(name, age, weight, height)
+        
+        #Validating password length
         while True:     
             password=input('Set a password for the new user: ')
             if len(password)<8:
                 print('\nPassword length is less than 8.')
             else:
                 break
-        new_user.set_password(password)    
+        #Appending new user info
+        new_user=User(name, age, weight, height, password=password)
         self.users[self.username] = new_user.to_dict()
-        print(f"\nUser {self.username} created successfully.\n")
-        self.df_users=pd.DataFrame(self.users).T
-        return self.users
+        
+        if self.df_users.shape == (0,0):
+            self.df_users = pd.DataFrame(self.users).T
+        else:
+            self.df_users.loc[self.username]=self.users[self.username]
     
-    #
-    def edit_user(self, username, **kwargs):
-        for user in self.users:
-            if user==username:
-                for key,value in kwargs.items:
-                    self.users[username][key]=value
+        print(f"\nUser {self.username} created successfully.\n")
+        
+        return self.username #To use in main
+    
+    #Edits user info in case of mistake
+    def edit_user_info(self, username, **kwargs):
+        for key,value in kwargs.items:
+            self.df_users.at[username,key] = value
     
     #Appending each workout dictionary in workout list            
     def add_workout(self, user, date, duration, calories_burned, type_, distance, reps, sets, description):
@@ -177,30 +169,30 @@ class FitnessTracker:
             self.workouts.append(RunWorkout(user, date, duration, calories_burned, description, distance).to_dict())
             print('\n',pd.DataFrame(RunWorkout(user, date, duration, calories_burned, description, distance).to_dict(), index=[0]).to_string(index=False))    
         
-        self.df_workouts = pd.DataFrame(self.workouts)    
+        #Appending new row to dataframe
+        self.df_workouts = self.df_workouts.append(self.workouts[-1])
+    
         print(f"\nWorkout added for user {user}\n")
 
-
     def edit_workout(self, username, date, description, **kwargs):
-        for workout in self.workouts:
-            if workout['user']==username and workout['date']==date and workout['description']==description:
+        
+        for index, row in self.df_workouts.iterrows():
+            if row['user'] == username and row['date'] == date and row['description'] == description:
                 for key, value in kwargs.items():
-                    workout[key]=value
+                    self.df_workouts.at[index,key] = value
                     print(f'\n{key.capitalize()} successfully updated with {value}\n')
                     break
-            elif workout == self.workouts[-1]:
+            elif index+1 == len(self.df_workouts):
                 print('\nNo changes have been made!\n')
                 
-        self.df_workouts = pd.DataFrame(self.workouts)
-        return self.workouts
-
+    
 
     def view_workouts(self, username):
         print('\n', self.df_workouts[self.df_workouts['user']==username].iloc[:,1:].to_string(index=False), '\n')
 
 
     def set_goal(self, username, goal):
-        self.users[username]['goal'] = goal
+        self.df_users.at[username, 'goal'] = goal
         print(f'\nGoal set for user {username}\n')
 
     def track_progress(self, username, type_, variables):
@@ -237,28 +229,40 @@ class FitnessTracker:
         
         
     def save_data(self, users_file="users.csv", workouts_file="workouts.csv"):
-        self.df_users.to_csv(users_file, mode='w')
+        
+        ##If loaded data is not empty headers are saved
+        
+        #Users data
+        if self.df_users.shape == (0,0):
+            self.df_users.to_csv(users_file, mode='a', header=True)
+        else:
+            self.df_users.to_csv(users_file, mode='a', header=False)
         print(f'Transformed data saved to {users_file}')
         
-        self.df_workouts.to_csv(workouts_file, mode='w')
+        #Workouts data
+        if self.df_workouts.shape == (0,0):
+            self.df_workouts.to_csv(workouts_file, mode='a', header=True)
+        else:
+            self.df_workouts.to_csv(workouts_file, mode='a', header=False)
         print(f'Transformed data saved to {workouts_file}\n')
         
         
     def load_data(self, users_file="users.csv", workouts_file="workouts.csv"):
         
         try:
-            with open(users_file, 'r'):
-                
-                print(f'Users data loaded from {users_file}\n')
+            df_users = pd.read_csv(users_file)
+            print(f'Users data loaded from {users_file}\n')
         except FileNotFoundError:
             print(f'File {users_file} not found\n')
         except Exception as e:
             print(f'An error occurred: {e}\n')
 
         try:
-            with open(workouts_file, 'r'):
-                print(f'Workouts data loaded from {workouts_file}\n')
+            df_workouts = pd.read_csv(workouts_file)
+            print(f'Workouts data loaded from {workouts_file}\n')
         except FileNotFoundError:
             print(f'File {workouts_file} not found.\n')
         except Exception as e:
             print(f"An error occurred: {e}")
+            
+        return df_users, df_workouts
--- main.py
+++ main.py
@@ -33,137 +33,187 @@ def main():
     tracker = FitnessTracker()
     tracker.load_data()
     
-    menu = {1: 'Create User', # +edit user info
-            2: 'Edit User Info',
-            3: 'Add Workout',  #აქ არ დაგავიწყდეს დაამატე, თუ იუზერი ემპტია
-            4: 'Edit Workout',
-            5: 'View Workouts',
-            6: 'Set Goals',
-            7: 'Track Progress',
-            8: 'Save Data to File',
-            9: 'Exit'}
+    menu = {
+            1: 'Edit User Info',  #from loaded df
+            2: 'Add Workout',   #from workout list ()
+            3: 'Edit Workout',  #
+            4: 'View Workouts',
+            5: 'Set Goals',
+            6: 'Track Progress',
+            7: 'Save Data to File',
+            8: 'Log out'
+            }
     
-
+    #In both case user will be logged in automatically
+    log = {1: 'Log in', 
+           2: 'Register', #create_user 
+           3: 'Exit'}
+    
+    #Main big loop
     while True:
-        #Prints menu iteratively after every function
-        for key, value in menu.items():
+        
+        #Printing menu for login
+        for key, value in log.items():
             print(key, value)
         
+        #Function input loop
         while True:
             function = input('\nChoose functionality: ')
             is_valid, function = validators.number_validator(function, int)
             if is_valid:
                 break
-              
-        if function == 1: #Create User
+        
+        #Login    
+        if function == 1:
+            while True:
+                username = input("Enter your username: ")
+                password = input("Enter your password: ")
+                if tracker.authenticate_user(username, password):
+                    break
+                else:
+                    print("Incorrect username or password. Please try again.")
+                    
+        #Register +login
+        elif function == 2:
             name = input('\nEnter your name: ')
             age = loops.num_loop('your age', validators.number_validator, num_type=int)
             weight = loops.num_loop('your weight', validators.number_validator, num_type = float)
             height = loops.num_loop('your height', validators.number_validator, num_type = int)
         
             #Creating new user
-            tracker.create_user(name, age, weight, height)
-                
-        elif function == 2: #Edit user info
-            user = loops.user_loop('your username', validators.user_validator, tracker.users)
-            
-            while True:    
-                fields = input('Enter variable (comma separated, e.g., weight,height): ').split(',')
-                for field in fields:
-                    if field not in ['name', 'age', 'weight','height', 'goal']:
-                        print('\nInvalid variables provided! Please enter from these - [name, age, weight, height, goal]\n')
-                        break
-                else:
-                    break
-                
-            fields = input('Enter the fields to edit (comma separated, e.g., weight,height): ').split(',')
-            updates = {field: input(f'Enter new value for {field}: ') for field in fields}
-            
-        elif function == 3: #Add Workout
-            
-            user = loops.user_loop('your username', validators.user_validator, tracker.users) 
-            date = loops.date_loop('workout date (in format dd/mm/yyyy)', validators.date_validator)
-            duration = loops.num_loop('workout duration (in minutes)', validators.number_validator, float)
-            calories_burned = loops.num_loop('calories you burned', validators.number_validator, int)
-            
-            while True:
-                type_ = input('Enter workout type (Cardio, Run, Strength): ').lower()
-                if type_ not in ['cardio', 'run', 'strength']:
-                    print('Invalid workout type')
-                else:
-                    break
-                
-            distance = loops.num_loop('distance (if applicable, else 0)', validators.number_validator, float)
-            reps = loops.num_loop('reps (if applicable, else 0)', validators.number_validator, int)
-            sets = loops.num_loop('sets (if applicable, else 0)', validators.number_validator, int)
-            description = input('Enter description: ')
-            
-            tracker.add_workout(user, date, duration, calories_burned, type_, distance, reps, sets, description)
-            
-        elif function == 4: #Edit Workout
-            user = loops.user_loop('your username', validators.user_validator, tracker.users) 
-            date = loops.date_loop('workout date (in format dd/mm/yyyy)', validators.date_validator)
-            description = input('Enter description: ')
-            
-            while True:    
-                fields = input('Enter variable (comma separated, e.g., duration,calories_burned): ').split(',')
-                for field in fields:
-                    if field not in ['date', 'duration', 'calories_burned','type', 'distance', 'reps', 'sets', 'description']:
-                        print('\nInvalid variables provided! Please enter from these - [date, duration, calories_burned, distance, reps, sets, description]\n')
-                        break
-                else:
-                    break
-                
-            updates = {field: input(f'Enter new value for {field}: ') for field in fields}
-            
-            tracker.edit_workout(user, date, description, **updates)
-            
-        elif function == 5: #View Workouts
-            user = loops.user_loop('your username', validators.user_validator, tracker.users)
-            tracker.view_workouts(user)
-            
-        elif function == 6: #Set Goals
-            user = loops.user_loop('your username', validators.user_validator, tracker.users)
-            goal = loops.num_loop('goal (in kg)', validators.number_validator, float)
-
-            tracker.set_goal(user, goal)
-            
-        elif function == 7: #Track Progress
-            user = loops.user_loop('your username', validators.user_validator, tracker.users)
-            
-            #makes sure type is correctly inputted
-            while True:
-                type_ = input('Enter workout type (Cardio, Run, Strength): ').lower()
-                if type_ not in ['cardio', 'run', 'strength']:
-                    print('Invalid workout type')
-                else:
-                    break
-                
-            #makes sure variables provided actually exist    
-            while True:    
-                variables = input('Enter variable (comma separated, e.g., duration,calories_burned): ').split(',')
-                for variable in variables:
-                    if variable not in ['duration', 'calories_burned', 'distance', 'reps', 'sets']:
-                        print('\nInvalid variables provided! Please enter from these - [duration, calories_burned, distance, reps, sets]\n')
-                        break
-                else:
-                    break
-            
-            #Displays and saves line chart of input variables
-            tracker.track_progress(user, type_, variables)
-            
-        elif function == 8: #Save Data to File
-            tracker.save_data()
-            
-        elif function == 9: #Exit
-            print('Saving data...')
-            tracker.save_data() #Saving data even in case of exit
-            print('Succesfully saved, exiting...\nGoodbye!')
+            username = tracker.create_user(name, age, weight, height)
+              
+        #Exit
+        elif function == 3:
+            print('Exiting...\nGoodbye\n')
             break
         
-        else:
-            print("Invalid choice, number not in (1-8). Please try again.")
-        
+        #Goes to inner loop (menu)
+        while True:
+            
+            #Printing menu
+            for key, value in menu.items():
+                print(key, value)
+            
+            #Function choosing loop
+            while True:
+                function = input('\nChoose functionality: ')
+                is_valid, function = validators.number_validator(function, int)
+                if is_valid:
+                    break
+                  
+            #Edit user info
+            if function == 1: 
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users)
+                
+                while True:    
+                    fields = input('Enter variable (comma separated, e.g., weight,height): ').split(',')
+                    for field in fields:
+                        if field not in ['name', 'age', 'weight','height', 'goal']:
+                            print('\nInvalid variables provided! Please enter from these - [name, age, weight, height, goal]\n')
+                            break
+                    else:
+                        break
+                    
+                fields = input('Enter the fields to edit (comma separated, e.g., weight,height): ').split(',')
+                updates = {field: input(f'Enter new value for {field}: ') for field in fields}
+                
+                #edit_user 
+            
+            #Add Workout
+            elif function == 2: #Add Workout
+                
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users) 
+                date = loops.date_loop('workout date (in format dd/mm/yyyy)', validators.date_validator)
+                duration = loops.num_loop('workout duration (in minutes)', validators.number_validator, float)
+                calories_burned = loops.num_loop('calories you burned', validators.number_validator, int)
+                
+                while True:
+                    type_ = input('Enter workout type (Cardio, Run, Strength): ').lower()
+                    if type_ not in ['cardio', 'run', 'strength']:
+                        print('Invalid workout type')
+                    else:
+                        break
+                    
+                distance = loops.num_loop('distance (if applicable, else 0)', validators.number_validator, float)
+                reps = loops.num_loop('reps (if applicable, else 0)', validators.number_validator, int)
+                sets = loops.num_loop('sets (if applicable, else 0)', validators.number_validator, int)
+                description = input('Enter description: ')
+                
+                tracker.add_workout(username, date, duration, calories_burned, type_, distance, reps, sets, description)
+                
+            #Edit Workout
+            elif function == 3: #Edit Workout
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users) 
+                date = loops.date_loop('workout date (in format dd/mm/yyyy)', validators.date_validator)
+                description = input('Enter description: ')
+                
+                while True:    
+                    fields = input('Enter variable (comma separated, e.g., duration,calories_burned): ').split(',')
+                    for field in fields:
+                        if field not in ['date', 'duration', 'calories_burned','type', 'distance', 'reps', 'sets', 'description']:
+                            print('\nInvalid variables provided! Please enter from these - [date, duration, calories_burned, distance, reps, sets, description]\n')
+                            break
+                    else:
+                        break
+                    
+                updates = {field: input(f'Enter new value for {field}: ') for field in fields}
+                
+                tracker.edit_workout(username, date, description, **updates)
+                
+            #View Workouts
+            elif function == 4:
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users)
+                tracker.view_workouts(username)
+                
+            #Set Goal
+            elif function == 5: 
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users)
+                goal = loops.num_loop('goal (in kg)', validators.number_validator, float)
+    
+                tracker.set_goal(username, goal)
+                
+            #Track Progress
+            elif function == 6:
+                #user = loops.user_loop('your username', validators.user_validator, tracker.users)
+                
+                #makes sure type is correctly inputted
+                while True:
+                    type_ = input('Enter workout type (Cardio, Run, Strength): ').lower()
+                    if type_ not in ['cardio', 'run', 'strength']:
+                        print('Invalid workout type')
+                    else:
+                        break
+                    
+                #makes sure variables provided actually exist    
+                while True:    
+                    variables = input('Enter variable (comma separated, e.g., duration,calories_burned): ').split(',')
+                    for variable in variables:
+                        if variable not in ['duration', 'calories_burned', 'distance', 'reps', 'sets']:
+                            print('\nInvalid variables provided! Please enter from these - [duration, calories_burned, distance, reps, sets]\n')
+                            break
+                    else:
+                        break
+                
+                #Displays and saves line chart of input variables
+                tracker.track_progress(username, type_, variables)
+                
+            #Save Data to File
+            elif function == 7:
+                print('Saving data...')
+                tracker.save_data()
+                
+            #Logout
+            elif function == 8: 
+                print('Saving data...')
+                tracker.save_data() #Saving data even in case of exit
+                print('Succesfully saved, exiting...\nGoodbye!')
+                break
+            
+            #Invalid choice
+            else:
+                print("Invalid choice, number not in (1-8). Please try again.")
+            
 if __name__ == "__main__":
     main()
     
